c8cdcf4752feb1f97eca1517a5d76ac5e4853b98
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2018-09-23 19:40:12 -0400

    remove old compose

--- init.sql
+++
@@ -1,7 +0,0 @@
-create extension "uuid-ossp";
-create table public.todos (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  done boolean not null default false,
-  task text not null,
-  due timestamptz
-);
